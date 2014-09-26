aa6509bb1cfa8f57eb17750002a2b55165c7be4b
farah88 <afzal@hiskp.uni-bonn.de>
2014-09-26 14:23:12 +0200

    changed the name of my beam time and the paths to my directories

--- CaLib/macros/WriteARCalibration.C
+++ CaLib/macros/WriteARCalibration.C
@@ -20,24 +20,24 @@ void WriteARCalibration()
     gSystem->Load("libCaLib.so");
     
     // write tagger calibration file
-    TCWriteARCalib w(kDETECTOR_TAGG, "FP.dat");
-    w.Write("new_FP.dat", "LD2_Dec_07", 13840);
+    //TCWriteARCalib w(kDETECTOR_TAGG, "/disk/user/afzal/Mainz/acqu/acqu_user/data/Tagger/Tagger.dat");
+    //w.Write("/disk/user/afzal/Mainz/acqu/acqu_user/data/Tagger/new_Tagger.dat", "2013_11_G-E_Linturi", 13840);
 
     // write CB calibration file
-    TCWriteARCalib w1(kDETECTOR_CB, "NaI.dat");
-    w1.Write("new_NaI.dat", "LD2_Dec_07", 13840);
+    TCWriteARCalib w1(kDETECTOR_CB, "/disk/user/afzal/Mainz/acqu/acqu_user/data/CB/CB_NaI.dat");
+    w1.Write("/disk/user/afzal/Mainz/acqu/acqu_user/data/CB/new_CB_NaI.dat", "2013_11_G-E_Linturi",467);
 
     // write TAPS calibration file
-    TCWriteARCalib w2(kDETECTOR_TAPS, "BaF2.dat");
-    w2.Write("new_BaF2.dat", "LD2_Dec_07", 13090);
+    //TCWriteARCalib w2(kDETECTOR_TAPS, "/disk/user/afzal/Mainz/acqu/acqu_user/data/TAPS/TAPS_BaF2-PbWO4.dat");
+    //w2.Write("/disk/user/afzal/Mainz/acqu/acqu_user/data/TAPS/new_TAPS_BaF2-PbWO4.dat", "2013_11_G-E_Linturi", 13090);
 
     // write PID calibration file
-    TCWriteARCalib w3(kDETECTOR_PID, "PID.dat");
-    w3.Write("new_PID.dat", "LD2_Dec_07", 13840);
+    //TCWriteARCalib w3(kDETECTOR_PID, "PID.dat");
+    //w3.Write("new_PID.dat", "2013_11_G-E_Linturi", 13840);
 
     // write Veto calibration file
-    TCWriteARCalib w4(kDETECTOR_VETO, "Veto.dat");
-    w4.Write("new_Veto.dat", "LD2_Dec_07", 13840);
+    //TCWriteARCalib w4(kDETECTOR_VETO, "Veto.dat");
+    //  w4.Write("new_Veto.dat", "2013_11_G-E_Linturi", 13840);
 
     gSystem->Exit(0);
 }
