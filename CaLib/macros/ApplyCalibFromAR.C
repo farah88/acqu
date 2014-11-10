4ccedce50f20d4479402b04a85bfd5981089b67d
farah88 <afzal@hiskp.uni-bonn.de>
2014-11-10 13:30:12 +0100

    changed path in the files accordingly

--- CaLib/macros/ApplyCalibFromAR.C
+++ CaLib/macros/ApplyCalibFromAR.C
@@ -4,21 +4,20 @@ void ApplyCalibFromAR() {
  
 	// macro configuration: just change here for your beamtime and leave
 	// the other parts of the code unchanged
-	const Int_t firstRun            = 4921;
-	const Int_t lastRun             = 6008;
-	const Char_t calibName[]        = "2014-07_EPT_Prod_Neiser";
-	const Char_t calibDesc[]        = "Calibration for July 2014 EPT Beamtime";
-	const Char_t calibFileTAPS[]    = "../acqu_user/data/Detector-BaF2-PbWO4-Cosmics.dat";
+	const Int_t firstRun            = 200;
+	const Int_t lastRun             = 1490;
+	const Char_t calibName[]        = "2013_11_G-E_Linturi";
+	const Char_t calibDesc[]        = "Standard calibration for butanol November 2013 beamtime";
+	const Char_t calibFile[]    = "/disk/user/afzal/Mainz/acqu/acqu_user/data/CB/PID_calib_nov_output7.dat";
 
 	// remove the to be overwritten data sets first
 	const Int_t runset = 0;
-	TCMySQLManager::GetManager()->RemoveDataSet("Data.TAPS.LG.E0", calibName, runset);
-	TCMySQLManager::GetManager()->RemoveDataSet("Data.TAPS.LG.E1", calibName, runset);
-	TCMySQLManager::GetManager()->RemoveDataSet("Data.TAPS.T1", calibName, runset);
+	TCMySQLManager::GetManager()->RemoveDataSet("Data.PID.E0", calibName, runset);
+	TCMySQLManager::GetManager()->RemoveDataSet("Data.PID.E1", calibName, runset);
 
 	
 	// read AcquRoot calibration of TAPS
-	TCMySQLManager::GetManager()->AddCalibAR(kDETECTOR_TAPS, calibFileTAPS,
+	TCMySQLManager::GetManager()->AddCalibAR(kDETECTOR_PID, calibFile,
 	                                         calibName, calibDesc,
 	                                         firstRun, lastRun);
  
